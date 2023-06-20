ae940adc619e44119a972d07142ab4b5d133c20c
TK_MINI <40619640+otm0937@users.noreply.github.com>
2023-06-20 18:14:30 +0900

    merge main, setup arduino

--- main.py
+++ main.py
@@ -1,51 +1,81 @@
-import time
-
-import cv2
 from ultralytics import YOLO
+import cv2
+import torch
+import serial
 
-model = YOLO('runs/detect/train/weights/best.pt')
+
+def transform_data(data):
+    x1, y1, x2, y2, conf, cls = torch.split(data, 1, dim=1)
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    transformed_data = torch.cat([x, y, cls], dim=1)
+    return transformed_data.to(torch.int)
+
+
+def get_min_x_data(data):
+    min_x_index = torch.argmin(data[:, 0])
+    min_x_data = data[min_x_index]
+    return min_x_data
+
+
+d_model = YOLO('runs/detect/train/weights/best.pt')
+y_model = YOLO('yolov8n.pt')
+
+ser = serial.Serial('COM3')
+print(ser.name)
 
 cap = cv2.VideoCapture(1)
 
 if cap.isOpened():
     print('width: {}, height : {}'.format(cap.get(3), cap.get(4)))
 
-prev_time = time.time()
-cnt = 0
-avg = 1.0
-max_fps = 0
-min_fps = 100000
+ser = serial.Serial('COM7',115200)
+print(ser.name)
 
 while True:
     ret, frame = cap.read()
 
     if ret:
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 0)
 
-        inputs = [frame]
-        results = model(inputs)
-        res_plotted = results[0].plot()
-        now_time = time.time()
+        y_result = y_model([frame])[0]
+        y_res_plot = y_result.plot()
+        y_data = y_result.boxes.data
+        is_person = torch.any(y_data[:, -1] == 0).item()
 
-        dly = now_time - prev_time
-        fps = (1 / dly)
-        avg = (avg * cnt + fps) / (cnt + 1)
-        cnt += 1
-        prev_time = now_time
+        result = '1' if is_person else '0'
 
-        if fps > max_fps:
-            max_fps = fps
-        if fps < min_fps:
-            min_fps = fps
+        d_result = d_model.predict(
+            source=[frame],
+            conf=0.65
+        )[0]
+        d_res_plot = d_result.plot()
+        d_data = d_result.boxes.data  # tensor([[x1,y1,x2,y2,conf,cls]]) torch.float32
 
-        cv2.putText(res_plotted, "cnt: %d" % cnt, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "dly: %.4f" % dly, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "fps: %.2f" % fps, (5, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "avg: %.2f" % avg, (5, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "max: %.2f" % max_fps, (5, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "min: %.2f" % min_fps, (5, 130), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        res_plot = cv2.hconcat([y_res_plot, d_res_plot])
+        cv2.putText(res_plot, str(is_person), (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.imshow("result", res_plot)
 
-        cv2.imshow("res", res_plotted)
+        if d_data.numel() == 0:
+            result = result + ',' + '0,0,0,0'
+        else:
+            result = result + ',' + '1'
+
+            data = transform_data(d_data)  # tensor([[x,y,cls]])
+
+            datum = get_min_x_data(data)
+
+            result = result + ',' + f"{datum[0]},{datum[1]},{datum[2]}\n"
+
+        print(result)
+
+        try:
+            ser.write(result.encode())
+        except:
+            print('error')
+            ser.close()
+            ser = serial.Serial('COM7')
+            print(ser.name)
 
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
@@ -54,6 +84,6 @@ while True:
         print('error')
         break
 
-
 cap.release()
+ser.close()
 cv2.destroyAllWindows()
